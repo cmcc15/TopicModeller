269feed5f33a7b176a6eb7d6f334ec42bcf0ab2c
cmcc15 <72230626+cmcc15@users.noreply.github.com>
2022-04-04 20:45:44 +0100

    Update Control.java

--- com.TopicModeller/src/Modeller/Control.java
+++ com.TopicModeller/src/Modeller/Control.java
@@ -1,20 +1,15 @@
 package Modeller;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Control {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-
-        Read toRead = new Read();
-        
-        toRead.main(args);//allows us to see the output of the files
-        
-        GUI guiInterface = new GUI();//allows us to see the GUI
-        
+		
+		TopicModeller handle = new TopicModeller();
+		
+		handle.toRead();//to read the files
+		handle.Uncommon();//to see uncommonon words
         
         	
     }
