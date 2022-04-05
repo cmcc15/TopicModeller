70f7d6d7f9ea87d9d97c47f03e63ffe10ccf44b8
cmcc15 <72230626+cmcc15@users.noreply.github.com>
2022-04-05 21:37:23 +0100

    Update Control.java

--- com.TopicModeller/src/Modeller/Control.java
+++ com.TopicModeller/src/Modeller/Control.java
@@ -1,22 +1,25 @@
 package Modeller;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Control {
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		TopicModeller handle = new TopicModeller();
 		
 		//handle.toReadFirst();//to read the files
 		//handle.toReadSecond();
 		
-		handle.toRead();
+		//handle.toRead();
 		//handle.Uncommon();//to see uncommonon words
 		//handle.Overlap();
 		//handle.addStop();
 		
-		//handle.Common();
+		handle.Remove();
+		
+		handle.Common();
         
 		
 		//GUI modGUI =new GUI();
@@ -26,3 +29,4 @@ public class Control {
 
 
 }
+
