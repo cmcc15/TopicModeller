24f547f5c32b6c785e7ecf46b7c634160f632220
cmcc15 <72230626+cmcc15@users.noreply.github.com>
2022-04-10 23:53:50 +0100

    Update TopicModeller.java

--- com.TopicModeller/src/Modeller/TopicModeller.java
+++ com.TopicModeller/src/Modeller/TopicModeller.java
@@ -10,7 +10,10 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class TopicModeller {
@@ -235,7 +238,31 @@ public class TopicModeller {
 		
 	}
 	
-	void Sort() {
+	void Sort() throws IOException {
+		
+		//sorts Alphabetically     
+		String inputFile = "Lakers.txt";
+		String outputFile = "Lakers.txt";
+
+		FileReader fileReader = new FileReader(inputFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String inputLine;
+		List<String> lineList = new ArrayList<String>();
+		while ((inputLine = bufferedReader.readLine()) != null) {
+			lineList.add(inputLine);
+		}
+		fileReader.close();
+
+		Collections.sort(lineList);
+
+		FileWriter fileWriter = new FileWriter(outputFile);
+		PrintWriter out = new PrintWriter(fileWriter);
+		for (String outputLine : lineList) {
+			out.println(outputLine);
+		}
+		out.flush();
+		out.close();
+		fileWriter.close();
 		
 	}
 	
