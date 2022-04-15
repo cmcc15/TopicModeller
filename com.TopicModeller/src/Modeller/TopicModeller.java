0930d9f3ce51fdf457343f6ab22005ac266aaba6
cmcc15 <72230626+cmcc15@users.noreply.github.com>
2022-04-15 19:45:00 +0100

    Update TopicModeller.java

--- com.TopicModeller/src/Modeller/TopicModeller.java
+++ com.TopicModeller/src/Modeller/TopicModeller.java
@@ -69,9 +69,10 @@ public class TopicModeller {
 		ArrayList<String> list1 = new ArrayList<String>();
 		        
 		while (file1.hasNextLine())
+		{
 			list1.add(file1.nextLine());
-		        
-		    
+		}
+		           
 		System.out.println(list1);//to read contents of the first document
 		        
 		Scanner myScanner2 = new Scanner(new File("PremierLeague.txt"));//this is for reading the second document
@@ -221,11 +222,11 @@ public class TopicModeller {
 	
 	void Remove () throws IOException {
 		//to remove stop words from the documents 
-        PrintWriter pw = new PrintWriter("PremierLeague.txt");
+        PrintWriter print = new PrintWriter("PremierLeague.txt");
           
-        BufferedReader br1 = new BufferedReader(new FileReader("ChampionsLeague.txt"));
+        BufferedReader buffer = new BufferedReader(new FileReader("ChampionsLeague.txt"));
           
-        String line1 = br1.readLine();
+        String line1 = buffer.readLine();
           
         while(line1 != null)
         {
@@ -247,17 +248,17 @@ public class TopicModeller {
             }//end while
               
             if(!flag)
-                pw.println(line1);
+            	print.println(line1);
               
-            line1 = br1.readLine();
+            line1 = buffer.readLine();
               
         }//end while
           
-        pw.flush();
+        print.flush();
           
         // closing resources
-        br1.close();
-        pw.close();
+        buffer.close();
+        print.close();
           
         System.out.println("File operation was successful");
     }//end Remove 
@@ -279,28 +280,87 @@ public class TopicModeller {
 			
 	}//end Overlap
 	
-	void MostCommon() throws FileNotFoundException {
+	void MostCommon() throws IOException {
 		//to find the most common words from the two documents
-		Scanner file1 = new Scanner(new File("ChampionsLeague.txt"));
-		ArrayList<String> list1 = new ArrayList<String>();
-		        
-		while (file1.hasNextLine())
-			list1.add(file1.nextLine());
-		        
-		Scanner myScanner2 = new Scanner(new File("PremierLeague.txt"));
-		
-		ArrayList<String> list2 = new ArrayList<String>();
-		
-		while (myScanner2.hasNextLine())
-			list2.add(myScanner2.nextLine());
-		
-		list1.addAll(list2);
-		
-		System.out.println(list1);
-		
-	}
-	
-	void Sort() throws IOException {
+		String line, word = "";    
+        int count = 0, maxCount = 0;    
+        ArrayList<String> document = new ArrayList<String>();    
+            
+        //Opens document  
+        FileReader docx = new FileReader("ChampionsLeague.txt");    
+        BufferedReader buffer = new BufferedReader(docx);    
+            
+        //Reads each line    
+        while((line = buffer.readLine()) != null) {    
+            String string[] = line.toLowerCase().split("([,.\\s]+) ");      
+            for(String s : string){    
+            	document.add(s);    
+            }    
+            
+        }    
+            
+        //Find the most repeated word in a document   
+        for(int i = 0; i < document.size(); i++){    
+            count = 1;      
+            for(int j = i+1; j < document.size(); j++){    
+                if(document.get(i).equals(document.get(j))){    
+                    count++;    
+                }     
+            }       
+            if(count > maxCount){    
+                maxCount = count;    
+                word = document.get(i);    
+            }    
+        }      
+        System.out.println("Most repeated word: " + word);    
+        buffer.close(); 
+        
+        //to find the most common words from the two documents
+        String line2, word2 = "";    
+        int count2 = 0, maxCount2 = 0;    
+        ArrayList<String> document2 = new ArrayList<String>();    
+                  
+        //Opens document   
+        FileReader docx2 = new FileReader("PremierLeague.txt");    
+        BufferedReader buffer2 = new BufferedReader(docx2);    
+                  
+              //Reads each line    
+              while((line2 = buffer2.readLine()) != null) {    
+                  String string[] = line2.toLowerCase().split("([,.\\s]+) ");        
+                  for(String s : string){    
+                	  document2.add(s);    
+                  }    
+                  
+              }    
+                  
+              //Find the most repeated word in a document    
+              for(int i = 0; i < document2.size(); i++){    
+                  count2 = 1;    
+                  
+                  for(int j = i+1; j < document2.size(); j++){    
+                      if(document2.get(i).equals(document2.get(j))){    
+                          count2++;    
+                      }     
+                  }    
+               
+                  if(count2 > maxCount2){    
+                      maxCount2 = count2;    
+                      word2 = document2.get(i);    
+                  }    
+              }    
+                  
+              System.out.println("Most repeated word: " + word2);    
+              buffer2.close(); 
+
+              int i=0;
+              
+			while (word != word2 & i <1) {
+				System.out.println("\n these words are not the same");  
+				i++;
+              }
+    } //end MostCommon 
+
+	void Sort1() throws IOException {
 		//sort the document
 		
 		//sorts Alphabetically     
@@ -327,45 +387,47 @@ public class TopicModeller {
 		out.close();
 		fileWriter.close();
 		
-	}//end Sort
+	}//end Sort1
 	
+	void Sort2() throws IOException {
+		//sort the document
+		
+		//sorts Alphabetically     
+		String inputFile = "ChampionsLeague.txt";
+		String outputFile = "ChampionsLeague.txt";
+
+		FileReader fileReader = new FileReader(inputFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String inputLine;
+		List<String> lineList = new ArrayList<String>();
+		while ((inputLine = bufferedReader.readLine()) != null) {
+			lineList.add(inputLine);
+		}
+		fileReader.close();
+
+		Collections.sort(lineList);
+
+		FileWriter fileWriter = new FileWriter(outputFile);
+		PrintWriter out = new PrintWriter(fileWriter);
+		for (String outputLine : lineList) {
+			out.println(outputLine);
+		}
+		out.flush();
+		out.close();
+		fileWriter.close();
+		
+	}//end Sort2
 	
-	void Save() {
-		String directory = System.getProperty("user.home");
-		String fileName = "Playstation.txt";
-		String absolutePath = directory + File.separator + fileName;
-
-		// Write the content in file 
-		try(FileWriter fileWriter = new FileWriter(absolutePath)) {
-		    String fileContent = "This is a sample text.";
-		    fileWriter.write(fileContent);
-		    fileWriter.close();
-		} catch (IOException e) {
-		    // Exception handling
-		}
-
-		// Read the content from file
-		try(FileReader fileReader = new FileReader(absolutePath)) {
-		    int read = fileReader.read();
-		    while(read != -1) {
-		        System.out.print((char)read);
-		        fileReader.close();
-		    }//end while
-		} catch (FileNotFoundException e) {
-		    
-		} catch (IOException e) {
-			
-		}
-	}//end Save
 	
 	void Add()  {
 		//to add to StopWords
 		try {
-		    Files.write(Paths.get("StopWords.txt"), "the text".getBytes(), StandardOpenOption.APPEND);
+		    Files.write(Paths.get("StopWords.txt"), "Test".getBytes(), StandardOpenOption.APPEND);
 		}catch (IOException e) {
 		    //exception handling left as an exercise for the reader
 		}
 	}//end Add
+	
 	
 
 }//end TopicModeller
